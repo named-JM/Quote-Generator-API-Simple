d2fc0b3a30cf67eab205e94af92a70bb80a0ec0d
Echel Cags <joannacaguco@gmail.com>
2024-03-26 22:42:11 +0800

    adds catch network error and too many request

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ async function fetchData() {
     };
 
     try {
+
+        //here need to intialize and fetch data  before respnise so it wont get error
         const response = await fetch(url, options);
+
+        if (!response.ok) {
+            if (response.status === 429) {
+                throw new Error('Too many requests. Please try again later.');
+            } else {
+                throw new Error('Network response was not ok');
+            }
+        }
+
+        
         const result = await response.json();
         const quoteContent = result.content;
         const quoteAuthor = result.originator.name;
@@ -25,6 +37,8 @@ async function fetchData() {
         
     } catch (error) {
         console.error(error);
+        const quoteContainer = document.getElementById('quote-content');
+        quoteContainer.textContent = 'Failed to fetch a quote. ' + error.message;
     }
 }
 
@@ -32,3 +46,5 @@ document.getElementById('newBtn').addEventListener('click', fetchData);
 
 // Fetch a quote when the page loads
 fetchData();
+
+
