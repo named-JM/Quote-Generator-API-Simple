97716eac1b57306d21b27312482068b65cf7ecb9
Echel Cags <joannacaguco@gmail.com>
2024-03-26 21:49:05 +0800

    origin

---
+++ script.js
@@ -0,0 +1,34 @@
+async function fetchData() {
+    const url = 'https://quotes15.p.rapidapi.com/quotes/random/';
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': '2543d99c85msh29c1dca6afd43eap1b84e4jsnbd18b03a0634',
+            'X-RapidAPI-Host': 'quotes15.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const result = await response.json();
+        const quoteContent = result.content;
+        const quoteAuthor = result.originator.name;
+        console.log(result);
+        console.log(quoteContent);
+        console.log(quoteAuthor);
+        
+        const quoteContainer = document.getElementById('quote-content');
+        const author = document.getElementById('author');
+
+        quoteContainer.textContent = quoteContent;
+        author.textContent = "- " + quoteAuthor;
+        
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+document.getElementById('new-quote-btn').addEventListener('click', fetchData);
+
+// Fetch a quote when the page loads
+fetchData();
